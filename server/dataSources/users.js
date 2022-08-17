47feb7325bc6e768b31b16be354789603e920d71
Barendan <barendan@gmail.com>
2022-08-17 11:46:55 -0400

    fix login mutation and clean code

--- server/dataSources/users.js
+++ server/dataSources/users.js
@@ -1,18 +1,65 @@
 import { MongoDataSource } from 'apollo-datasource-mongodb';
 import { ApolloError } from 'apollo-server-errors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export default class Users extends MongoDataSource {
     async getUser(id) {
         return await this.model.findById(id);
     }
 
-    async registerUser({username, email, password, token}) {
+    async registerUser({ registerInput: {username, email, password} }) {
         const oldUser = await this.model.findOne({ email });
+
         if (oldUser) {
             throw new ApolloError('A user is already registered with the email' + email, 'USER_ALREADY_EXISTS');
         }
 
+        let encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = {
+            username: username,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        }
+
+        const token = jwt.sign(
+            { user_id: newUser._id, email },
+            "UNSAFE_STRING",
+            {
+                expiresIn: "2h"
+            }
+        );
+        newUser.token = token;
+
+        // return {
+        //     id: res.id,
+        //     ...res._doc
+        // }
+
         return await this.model.create({ username, email, password, token });
     }
 
+    async loginUser({ loginInput: { email, password } }) { 
+        const user = await this.model.findOne({ email });
+            
+        if ( user && (await bcrypt.compare(password, user.password)) ) {
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                "UNSAFE_STRING",
+                {
+                    expiresIn: "2h"
+                }
+            );
+
+            user.token = token;
+
+            return {
+                id: user.id,
+                ...user._doc
+            }
+        } else {
+            throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD')
+        }
+    }
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,3 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 
 export const resolvers = {
     Query: {
@@ -14,59 +12,11 @@ export const resolvers = {
         }
     },
     Mutation: {
-        registerUser: async (_, {registerInput: {username, email, password} }, { dataSources: { users } }) => {
-
-            let encryptedPassword = await bcrypt.hash(password, 10);
-
-            const newUser = {
-                username: username,
-                email: email.toLowerCase(),
-                password: encryptedPassword
-            }
-
-            const token = jwt.sign(
-                { user_id: newUser._id, email },
-                "UNSAFE_STRING",
-                {
-                    expiresIn: "2h"
-                }
-            );
-
-            newUser.token = token;
-            
-            
-            const res = await users.registerUser(newUser)
-            console.log('reggie:', res)
-            
-            // const res = await newUser.save();
-
-            return {
-                id: res.id,
-                ...res._doc
-            }
+        registerUser: async (_, _args, { dataSources: { users } }) => {            
+            return users.registerUser(_args)
         },
-        loginUser: async (_, _args, { dataSources: { users } }) => {
-            const user = await this.model.findOne({ email });
-
-            if (user && (await bcrypt.compare(password, user.password))) {
-                const token = jwt.sign(
-                    { user_id: user._id, email },
-                    "UNSAFE_STRING",
-                    {
-                        expiresIn: "2h"
-                    }
-                );
-
-                user.token = token;
-
-                return {
-                    id: user.id,
-                    ...user._doc
-                }
-            } else {
-                throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD')
-            }
-
+        loginUser: (_, _args, { dataSources: { users } }) => {
+            return users.loginUser(_args)
         },
         createPost: (_root, _args, { dataSources: { posts } }) => {
             return posts.createPost(_args)
